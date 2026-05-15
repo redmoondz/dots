eefa0ed98e89dab5ec53026a5300a85963c9f33b
Stanislav <rublivsky@gmail.com>
2026-05-15 17:18:29 +0300

    clear trash

---
+++ scripts/sync_config.sh
@@ -0,0 +1,7 @@
+cp -r ~/.config/alacritty ./
+cp -r ~/.config/hypr ./
+cp -r ~/.config/noctalia ./ 
+cp -r ~/.config/solaar ./
+cp -r ~/.config/fastfetch/ ./ 
+cp -r ~/.zshrc ./
+cp -r ~/Pictures/Wallpaper ./
